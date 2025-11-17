dea17e7a9c5c82e05b886663d272aa9f8e0e7258
chris-reeves1 <chris.reeves@qa.com>
2025-11-17 12:24:27 +0000

    2

--- src/test/java/CalcTest.java
+++ src/test/java/CalcTest.java
@@ -1,13 +1,19 @@
+import static org.junit.jupiter.api.Assertions.assertAll;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInstance;
-import org.junit.jupiter.api.Assertions.*;
+//import org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import Rtesting.Calculator;
 
@@ -38,6 +44,58 @@ public class CalcTest {
         // Assert
         assertEquals(5, result);
     }
+
+    @ParameterizedTest // mainly primitive types
+    @ValueSource(ints = {0, 1, -1})
+    void add_with_zero_one_minus_one(int x){
+        assertEquals(x, calc.add(x, 0));
+    }
+
+    @ParameterizedTest(name = "add({0}, {1}) = {2}")
+    @CsvSource({"0, 0, 0",
+                "1, 0, 1",
+                "-1, 1, 0",
+                "100, 200, 300"                    
+})
+void add_table(int a, int b, int expected){
+    assertEquals(expected, calc.add(a, b));
+}
+
+@Test
+void divsion_throws_its_error(){
+    IllegalArgumentException x = assertThrows(IllegalArgumentException.class, () -> calc.div(10, 0));
+    assertEquals("b must not be zero", x.getMessage());
+}
+
+
+@Nested
+class ClampTests{
+
+    @Test
+    void clamp_is_less_than_min(){
+        assertEquals(10, calc.clamp(10, 5, 20));
+    }
+
+    @Test
+    void itthrowws(){
+        assertThrows(IllegalArgumentException.class, () -> calc.clamp(10, 30, 20));
+    }
+
+    @Test
+    void multiple_asserts(){
+        assertAll(
+            () -> assertEquals(10, calc.clamp(5, 10, 20)),
+            () -> assertEquals(10, calc.clamp(5, 10, 30))
+        );
+    }
+}
+
+
+
+
+
+
+
 
 
 
